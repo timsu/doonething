b644c357c61827963fe0c890e011dd92ce8dd324
Tim Su <timsu@yahoo-inc.com>
2015-01-26 00:33:44 -0800

    refactor already

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,17 @@ class User < ActiveRecord::Base
   has_secure_password
 
   belongs_to :active_task, :class => Task
-
-  def find_next_task
-    Task.where(:user => self, :completed_at => nil).order("importance DESC, created_at ASC").limit(1).first
+  
+  def find_next_task(options = {})
+    query = Task.where(:user => self, :completed_at => nil)
+    if options[:skip_task]
+      query = query.where("id != ?", options[:skip_task])
+    end
+    query.order("importance DESC, random(), created_at ASC").limit(1).first
   end
 
   def skip_task(task)
-    Task.where(:user => self, :completed_at => nil).
-      where("id != ?", task.id).order("importance DESC, created_at ASC").limit(1).first
+    find_next_task :skip_task => task.id
   end
 
   def importances
